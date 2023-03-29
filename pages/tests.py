fbc1d4d4d52e4c45cf9b3a4e20e988106df2b88a
acidburn <cc.acidburn@gmail.com>
2023-03-29 19:40:26 +0200

    tests for homepage

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 
+from .views import HomePageView
 class HomepageTests(SimpleTestCase):
 
     def setUp(self):
@@ -21,3 +22,10 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_does_not_containt_incorrect_html(self):
         self.assertNotContains(self.response, 'Hi there, I should not be on this page')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
