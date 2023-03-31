97fc1fc095d12a7d36ebafc47187bd81f73b11ec
acidburn <cc.acidburn@gmail.com>
2023-03-31 20:08:49 +0200

    test for signup

--- users/views.py
+++ users/views.py
@@ -7,4 +7,4 @@ from .forms import CustomUserCreationForm
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'auth/signup.html'
+    template_name = 'signup.html'
